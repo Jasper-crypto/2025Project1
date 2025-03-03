5d7382dc6441afea9231c45664bbc1d1cd180d47
Jasper MANAU <manaujasper003@gmail.com>
2025-03-03 17:57:10 +1000

    Add files via upload

---
+++ Read_File.py
@@ -0,0 +1,10 @@
+import pandas
+#print("hello Motherfuckers")
+#f= open("C:\\Users\\jasper manau\\Documents\\2025 Academic Year\\SEMESTER 1\\MC411 Software Engineering\\Wk2 The Software Power\\madang.csv","r")
+#print(f.read())
+
+ # Jasper MANAU 220553 @ DWU
+madangFiles =pandas.read_csv("C:\\Users\\jasper manau\\Documents\\2025 Academic Year\\SEMESTER 1\\MC411 Software Engineering\\Wk2 The Software Power\\madang.csv")
+
+print(madangFiles)
+
